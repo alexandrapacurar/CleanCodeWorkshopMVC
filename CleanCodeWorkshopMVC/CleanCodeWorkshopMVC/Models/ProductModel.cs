19ba14a42ec1ff8b723c0142b0724e5ba3682013
alexandrapacurar <alexandra.pacurar@fortech.ro>
2017-03-25 23:40:08 +0200

    CleanCodeWorkshopMVC
    CleanCodeWorkshopMVC

---
+++ CleanCodeWorkshopMVC/CleanCodeWorkshopMVC/Models/ProductModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CleanCodeWorkshopMVC.Models
+{
+  public class ProductModel
+  {
+    [Required]
+    [Display(Name = "Product Name")]
+    public string ProductName { get; set; }
+
+    [Required]
+    [Display(Name = "Product Price")]
+    public decimal Price { get; set; }
+
+    public int ProductId { get; set; }
+  }
+}
---
+++ CleanCodeWorkshopMVC/CleanCodeWorkshopMVC/Scripts/CustomScripts/products.js
@@ -0,0 +1,12 @@
+﻿var cls = document.getElementById("products").getElementsByTagName("td");
+var sum = 0;
+for (var i = 0; i < cls.length; i++) {
+    if (cls[i].className == "price") {
+        sum += isNaN(cls[i].innerHTML) ? 0 : parseInt(cls[i].innerHTML);
+    }
+}
+var div = document.getElementById("total");
+if (sum != 0) {
+    div.style.backgroundColor = "lightblue";
+}
+div.innerHTML = sum;
