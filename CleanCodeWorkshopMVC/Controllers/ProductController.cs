97326ca6d24bb3f29f62556694bf65453ad2657e
alexandrapacurar <alexandra.pacurar@fortech.ro>
2017-03-26 11:46:21 +0300

    New commit
    Commit

---
+++ CleanCodeWorkshopMVC/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CleanCodeWorkshopMVC.Models;
+
+namespace CleanCodeWorkshopMVC.Controllers
+{
+  public class ProductController : Controller
+  {
+    private static Products products = new Products();
+    public ActionResult CreatePRoduct()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult CreatePRoduct(ProductModel product)
+    {
+      products.CreatePRoduct(product);
+      return View("Index", products.products);
+    }
+
+    public ActionResult Index()
+    {
+      return View(products.products);
+    }
+  }
+
+  public class Products
+  {
+    public List<ProductModel> products = new List<ProductModel>();
+
+    public Products()
+    {
+      products.Add(new ProductModel
+      {
+        ProductName = "Apple",
+        Price = 2000
+      });
+
+      products.Add(new ProductModel
+      {
+        ProductName = "Samsung",
+        Price = 1500
+      });
+    }
+
+    public void CreatePRoduct(ProductModel product)
+    {
+      products.Add(product);
+    }
+
+    public void UpdatePRoduct(ProductModel product)
+    {
+      foreach (var productItem in products)
+      {
+        if (productItem.ProductName == product.ProductName)
+        {
+          products.Remove(productItem);
+          products.Add(product);
+        }
+      }
+    }
+  }
+}
---
+++ CleanCodeWorkshopMVC/Scripts/CustomScripts/calculator.js
@@ -0,0 +1,55 @@
+﻿// Get all the keys from document
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+
+// Add onclick event to all the keys and perform operations
+for (var i = 0; i < keys.length; i++) {
+    keys[i].onclick = function (e) {
+        // Get the input and button values
+        var input = document.querySelector('.screen');
+        var inputVal = input.innerHTML;
+        var btnVal = this.innerHTML;
+
+        // Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+        // If clear key is pressed, erase everything
+        if (btnVal == 'C') {
+            input.innerHTML = '';
+            decimalAdded = false;
+        }
+
+            // If eval key is pressed, calculate and display the result
+        else if (btnVal == '=') {
+            var equation = inputVal;
+            var lastChar = equation[equation.length - 1];
+
+            // Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+
+            // Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+            if (operators.indexOf(lastChar) > -1 || lastChar == '.')
+                equation = equation.replace(/.$/, '');
+
+            if (equation)
+                input.innerHTML = eval(equation);
+
+            decimalAdded = false;
+        }
+
+            // Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
+        else if (btnVal == '.') {
+            if (!decimalAdded) {
+                input.innerHTML += btnVal;
+                decimalAdded = true;
+            }
+        }
+
+            // if any other key is pressed, just append it
+        else {
+            input.innerHTML += btnVal;
+        }
+
+        // prevent page jumps
+        e.preventDefault();
+    }
+}
